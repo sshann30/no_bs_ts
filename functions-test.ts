ff55620f3c70f4b9d8a8bae46d8fa5ab814c03e6
unknown <sshann30@gmail.com>
2022-07-23 18:13:15 -0400

    moving on from getName error

---
+++ functions-test.ts
@@ -0,0 +1,7 @@
+import addNumbers, { addStrings } from "./functions"
+
+console.log(addNumbers(1,2));
+console.log(addStrings("a","b"));
+console.log(addStrings("a"));
+
+console.log(getName({first:'Steve', last:'Shannon'})); 
